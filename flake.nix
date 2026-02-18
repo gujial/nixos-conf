4b1390eb74abbd944597d90a35f9f9fae3312055
gujial <gu18647403665@outlook.com>
2026-02-18 20:31:09 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,129 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # home-manager, used for managing user coniguration
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    re3-flake = {
+      url = "github:gujial/re3-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    tinyMediaManager-flake = {
+      url = "github:gujial/tinyMediaManager-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    lazyvim-flake = {
+      url = "github:gujial/lazyvim-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    wechat-devtools.url = "github:MaikoTan/wechat-devtools";
+    txdedit.url = "github:gujial/txdedit";
+  };
+
+  outputs =
+    inputs@{
+      nixpkgs,
+      home-manager,
+      lanzaboote,
+      nur,
+      re3-flake,
+      tinyMediaManager-flake,
+      wechat-devtools,
+      txdedit,
+      lazyvim-flake,
+      ...
+    }:
+    {
+      nixosConfigurations = {
+        laptop-gu = nixpkgs.lib.nixosSystem {
+          modules = [
+            ./configuration.nix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs = { inherit inputs; };
+                users.gujial = import ./home.nix;
+              };
+              nixpkgs.overlays = [ nur.overlays.default ];
+            }
+
+            lanzaboote.nixosModules.lanzaboote
+            (
+              { pkgs, lib, ... }:
+              {
+                environment.systemPackages = [
+                  # For debugging and troubleshooting Secure Boot.
+                  pkgs.sbctl
+                ];
+
+                # Lanzaboote currently replaces the systemd-boot module.
+                # This setting is usually set to true in configuration.nix
+                # generated at installation time. So we force it to false
+                # for now.
+                boot.loader.systemd-boot.enable = lib.mkForce false;
+
+                boot.lanzaboote = {
+                  enable = true;
+                  pkiBundle = "/var/lib/sbctl";
+                };
+              }
+            )
+
+            lazyvim-flake.nixosModules.lazyvim
+
+            (
+              { pkgs, ... }:
+              {
+                nixpkgs.overlays = [
+                  nur.overlays.default
+                ];
+
+                environment.systemPackages = [
+                  re3-flake.packages.${pkgs.stdenv.hostPlatform.system}.reVC
+                  re3-flake.packages.${pkgs.stdenv.hostPlatform.system}.reVC-Improved
+                  tinyMediaManager-flake.packages.${pkgs.stdenv.hostPlatform.system}.default
+                  wechat-devtools.packages.${pkgs.stdenv.hostPlatform.system}.default
+                  txdedit.packages.${pkgs.stdenv.hostPlatform.system}.default
+                ];
+              }
+            )
+
+            (
+              { pkgs, ... }:
+              {
+                fonts.packages = [
+                  pkgs.nur.repos.rewine.ttf-wps-fonts
+                  pkgs.nur.repos.rewine.ttf-ms-win10
+                ];
+              }
+            )
+          ];
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,130 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  home = {
+    # 注意修改这里的用户名与用户目录
+    username = "gujial";
+    homeDirectory = "/home/gujial";
+
+    # 直接将当前文件夹的配置文件，链接到 Home 目录下的指定位置
+    # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+    # 递归将某个文件夹中的文件，链接到 Home 目录下的指定位置
+    # home.file.".config/i3/scripts" = {
+    #   source = ./scripts;
+    #   recursive = true;   # 递归整个文件夹
+    #   executable = true;  # 将其中所有文件添加「执行」权限
+    # };
+
+    # 直接以 text 的方式，在 nix 配置文件中硬编码文件内容
+    # home.file.".xxx".text = ''
+    #     xxx
+    # '';
+
+    # 通过 home.packages 安装一些常用的软件
+    # 这些软件将仅在当前用户下可用，不会影响系统级别的配置
+    # 建议将所有 GUI 软件，以及与 OS 关系不大的 CLI 软件，都通过 home.packages 安装
+
+    sessionVariables = {
+      LD_LIBRARY_PATH = /run/current-system/sw/share/nix-ld/lib;
+      IDEA_VM_OPTIONS = "/home/gujial/jetbra/vmoptions/idea.vmoptions";
+      CLION_VM_OPTIONS = "/home/gujial/jetbra/vmoptions/clion.vmoptions";
+      PYCHARM_VM_OPTIONS = "/home/gujial/jetbra/vmoptions/pycharm.vmoptions";
+      GOLAND_VM_OPTIONS = "/home/gujial/jetbra/vmoptions/goland.vmoptions";
+      DATAGRIP_VM_OPTIONS = "/home/gujial/jetbra/vmoptions/datagrip.vmoptions";
+      RUSTROVER_VM_OPTIONS = "/home/gujial/jetbra/vmoptions/rustrover.vmoptions";
+      RIDER_VM_OPTIONS = "/home/gujial/jetbra/vmoptions/rider.vmoptions";
+    };
+
+    packages = with pkgs; [
+      fastfetch
+      gimp
+      wechat-uos
+      obs-studio
+      qq
+      mpv
+      telegram-desktop
+      obsidian
+      adwsteamgtk
+      osu-lazer-bin
+      android-studio
+      prismlauncher
+      ventoy-full-qt
+      piper
+      fluent-reader
+      nixfmt
+      nur.repos.xddxdd.dingtalk
+      wemeet
+      splayer
+      darktable
+      scanmem
+      chromium
+      yt-dlp
+      scrcpy
+      ffmpeg
+      statix
+      feishu
+      python3
+      wpsoffice-cn
+      traceroute
+      android-tools
+      godot
+      firefox-bin
+      jadx
+      protonplus
+      gdb
+      gcc
+      jetbrains.idea
+      cutter
+      github-copilot-cli
+
+      kdePackages.spectacle
+      kdePackages.kcalc
+      kdePackages.filelight
+      kdePackages.isoimagewriter
+      kdePackages.kmousetool
+      kdePackages.kdenlive
+      okteta
+      kdePackages.kamoso
+      kdePackages.krecorder
+      kdePackages.kwave
+      supergfxctl-plasmoid
+      kdiff3
+      kdePackages.sweeper
+      kdePackages.ksystemlog
+      kdePackages.sddm-kcm
+      kdePackages.kpat
+      kdePackages.discover
+    ];
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "25.05";
+  };
+  programs = {
+
+    vscode.enable = true;
+
+    # git 相关配置
+    git = {
+      enable = true;
+      signing.key = "DDC9F70191CA14A3";
+      signing.signByDefault = true;
+      lfs.enable = true;
+      settings = {
+        user.name = "gujial";
+        user.email = "gu18647403665@outlook.com";
+        safe.directory = "/etc/nixos";
+      };
+    };
+  };
+}
