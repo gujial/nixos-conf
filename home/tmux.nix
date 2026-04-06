f8e28faccd1f6c887c8b643e494cf989f00b3432
gujial <gu18647403665@outlook.com>
2026-04-06 13:55:53 +0800

    更新 tmux 配置

--- home/tmux.nix
+++ home/tmux.nix
@@ -3,12 +3,6 @@
 {
   programs.tmux = {
     enable = true;
-    terminal = "tmux-256color";
-    historyLimit = 10000;
-    keyMode = "vi";
-    prefix = "C-a";
-    escapeTime = 0;
-    mouse = true;
 
     plugins = with pkgs.tmuxPlugins; [
       sensible
@@ -30,10 +24,6 @@
     ];
 
     extraConfig = ''
-      # 分屏快捷键
-      bind | split-window -h -c "#{pane_current_path}"
-      bind - split-window -v -c "#{pane_current_path}"
-      # 面板切换（vim 风格）
       bind h select-pane -L
       bind j select-pane -D
       bind k select-pane -U
